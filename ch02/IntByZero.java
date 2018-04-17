f705466183bc5b4c6c47283e4e0e90c2da8bdeb6
douglasjd1 <douglasjd09@gmail.com>
2018-04-17 15:39:48 -0500

    First Variable

---
+++ ch02/IntByZero.java
@@ -0,0 +1,11 @@
+public class IntByZero
+{
+    public static void main(String[] args)
+    {
+        int first = 42;
+        int second = 0;
+        int result = 42/0;
+
+        System.out.println(result);
+    }
+}

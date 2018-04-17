7b036ee96964f5471f48eb3ff8018dfaa534d210
douglasjd1 <douglasjd09@gmail.com>
2018-04-17 15:35:47 -0500

    First Variable

---
+++ ch02/IntExtremes.java
@@ -0,0 +1,20 @@
+public class IntExtremes
+{
+    public static void main(String[] args)
+    {
+        int positiveInt =  2147483647;
+        System.out.println(positiveInt);
+
+        positiveInt++;
+
+        System.out.println(positiveInt);
+
+        int negativeInt = -2147483648;
+        System.out.println(negativeInt);
+
+        negativeInt--;
+
+        System.out.println(negativeInt);
+
+    }
+}

4b92fc1578a773cc431832695b92c786151a83ca
douglasjd1 <douglasjd09@gmail.com>
2018-04-17 15:42:44 -0500

    doublebyzero

---
+++ ch02/DoubleByZero.java
@@ -0,0 +1,12 @@
+public class DoubleByZero
+{
+    public static void main(String[] args)
+    {
+        double first = 42.0;
+        double second = 0.0;
+        double result = first/second;
+
+        System.out.println(result);
+
+    }
+}

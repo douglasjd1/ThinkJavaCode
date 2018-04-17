c7d3885cb4a72c01b0d5876e3feb8a29206981dd
douglasjd1 <douglasjd09@gmail.com>
2018-04-17 15:03:51 -0500

    Date

---
+++ ch02/Date.java
@@ -0,0 +1,15 @@
+public class Date
+{
+    public static void main(String[] args)
+    {
+        String day = "Tuesday";
+        int date = 17;
+        String month = "April";
+        int year = 2018;
+
+        System.out.println("American format:");
+        System.out.println(day + ", " + month + " " + date + ", " + year);
+        System.out.println("European format:");
+        System.out.println(day + " " + date + " " + month + " " + year);
+    }
+}

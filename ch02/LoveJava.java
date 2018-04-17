0bd9650ecdf4fc467ee4e3e9fb9a9c0b4830f16f
douglasjd1 <douglasjd09@gmail.com>
2018-04-17 15:46:50 -0500

    I Love Java

---
+++ ch02/LoveJava.java
@@ -0,0 +1,12 @@
+public class LoveJava
+{
+    public static void main(String[] args)
+    {
+        System.out.println("I love Java!");
+        String one = "I ";
+        String two = "love ";
+        String three = "Java!";
+
+        System.out.println(one + two + three );
+    }
+}

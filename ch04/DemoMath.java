93a3785e1af8179e0e55ef9f5e87b354f64e095b
douglasjd1 <douglasjd09@gmail.com>
2018-04-18 16:06:51 -0500

    DemoMath

---
+++ ch04/DemoMath.java
@@ -0,0 +1,21 @@
+public class DemoMath
+{
+    public static void main(String[] args)
+    {
+        int a = -4;
+        int b = 8;
+        double c = 3.5;
+
+        System.out.println("Value of  int a: " + a);
+        System.out.println("Absolute value of int a: " + Math.abs(a));
+
+        System.out.println("Value of int a: " + a);
+        System.out.println("Value of int b: " + b);
+        System.out.println("Max value of int a and int b: " + Math.max(a,b));
+
+        System.out.println("Value of double c: " + c);
+        System.out.println("Double c to the power of c: " + Math.pow(c,c));
+
+        System.out.println("Value of pi: " + Math.PI);
+    }
+}

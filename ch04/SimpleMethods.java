054f2d44a9c13b7eca518194c6bc53eaa6fdf320
douglasjd1 <douglasjd09@gmail.com>
2018-04-18 15:57:14 -0500

    SimpleMethods

---
+++ ch04/SimpleMethods.java
@@ -0,0 +1,31 @@
+public class SimpleMethods
+{
+    public static void main(String[] args)
+    {
+        printCount(5);
+
+        printSum(4,6);
+        printSum(7,2);
+
+        printBoolean(true);
+        printBoolean(false);
+
+    }
+
+    public static void printCount(int count)
+    {
+        System.out.println("The count is: " + count);
+    }
+
+    public static void printSum(int x, int y)
+    {
+        System.out.println(x + " + " + y + " = " + (x+y));
+    }
+
+    public static void printBoolean(boolean isStudent)
+    {
+        System.out.println("I am a student: " + isStudent);
+    }
+
+}
+

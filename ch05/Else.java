b970844b909904164f446cdb21b9e3d3ac2397f1
douglasjd1 <douglasjd09@gmail.com>
2018-04-19 11:26:27 -0500

    Else

---
+++ ch05/Else.java
@@ -0,0 +1,18 @@
+public class Else
+{
+    public static void main(String[] args)
+    {
+        int hrs = 15;
+
+        if(hrs<13)
+        {
+            System.out.println("Good morning: " + hrs);
+        }
+
+        else
+        {
+            System.out.println("Good evening: " + hrs);
+        }
+
+    }
+}

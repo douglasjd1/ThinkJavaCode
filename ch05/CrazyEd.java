3f2fc6483e695a7b6e43ee53745af6c42df3a67b
douglasjd1 <douglasjd09@gmail.com>
2018-04-19 13:36:29 -0500

    CrazyEd

---
+++ ch05/CrazyEd.java
@@ -0,0 +1,81 @@
+import java.util.Scanner;
+
+public class CrazyEd
+{
+    public static void main(String[] args)
+    {
+        final int oneInchCost = 2;
+        final int twoInchCost = 4;
+        final int threeInchCost = 6;
+        final int oneInchShip = 2;
+        final int twoInchShip = 2;
+        final int threeInchShip = 4;
+        final int handlingCharge = 5;
+        int initialCost = 0;
+        int totalShip = 0;
+        int totalCost = 0;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Welcome to Crazy Ed's Sting Cheese. Would you like your string cheese to have a diameter of ");
+        System.out.println(" 1 inch, 2 inches, or 3 inches (enter 1, 2, or 3)?");
+
+        int cheeseSize = input.nextInt();
+
+        if(cheeseSize > 3 || cheeseSize < 1 )
+        {
+            System.out.println("This order is too crazy");
+        }
+
+        else
+        {
+            System.out.print("OK, so you want the diameter of your string cheese to be " + cheeseSize + ".");
+            System.out.println(" How many yards would you like to order?");
+
+            int cheeseLength = input.nextInt();
+
+            System.out.println("So, you want " + cheeseLength + " yards of " + cheeseSize + " inch diameter string cheese.");
+
+            if(cheeseSize == 1)
+            {
+
+                initialCost = (oneInchCost * cheeseLength);
+                totalShip = (oneInchShip * cheeseLength);
+            }
+
+            else if(cheeseSize == 2)
+            {
+
+                initialCost = (twoInchCost * cheeseLength);
+                totalShip = (twoInchShip * cheeseLength);
+            }
+
+            else if(cheeseSize == 3)
+            {
+
+                initialCost = (threeInchCost * cheeseLength);
+                totalShip = (threeInchShip * cheeseLength);
+            }
+
+            if(cheeseSize == 1 && cheeseLength > 50)
+            {
+                totalShip = 0;
+            }
+
+            if(cheeseSize == 2 && cheeseLength > 75)
+            {
+                totalShip = 0;
+            }
+
+            if(cheeseSize == 3 && cheeseLength > 25)
+            {
+                totalShip = 0;
+            }
+
+            totalCost = initialCost + totalShip + handlingCharge;
+
+            System.out.println("Your total, including shipping and handling, is: $" + totalCost);
+
+        }
+    }
+}

e3ccf9598215804f75f0e4adb6adfe42d86bc715
douglasjd1 <douglasjd09@gmail.com>
2018-04-19 11:15:26 -0500

    Condition

---
+++ ch05/Condition.java
@@ -0,0 +1,14 @@
+public class Condition
+{
+    public static void main(String[] args)
+    {
+        int num1 = 1357;
+        int num2 = 2468;
+
+        String result = (num1 % 2 != 0)? "Odd" : "Even";
+        System.out.println(num1 + " is " + result);
+
+        result = (num2 % 2 != 0)? "Odd" : "Even";
+        System.out.println(num2 + " is " + result);
+    }
+}

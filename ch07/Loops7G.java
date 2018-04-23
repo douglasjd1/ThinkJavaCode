6d6a0a6185946d15acc0861bbb8e6dfa0f7bed5e
douglasjd1 <douglasjd09@gmail.com>
2018-04-23 14:45:30 -0500

    Loops 7G

---
+++ ch07/Loops7G.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Loops7G
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter a number:");
+        int maxInput = input.nextInt();
+        System.out.print(0 + " | ");
+
+        for(int i = 1; i <= maxInput; i++)
+        {
+            System.out.printf("%5d", i);
+        }
+        System.out.println();
+        System.out.print("- \t");
+
+        for(int i = 1; i <= maxInput; i++)
+        {
+            System.out.print("-----");
+        }
+
+        System.out.println();
+        for(int i = 1; i <= maxInput; i++)
+        {
+            if(i<10)
+                System.out.print(i + "  | ");
+
+            else
+                System.out.print(i + " | ");
+            for(int j = 1; j <= maxInput ; j++)
+            {
+                System.out.printf("%5d", i * j);
+            }
+            System.out.println();
+        }
+
+    }
+
+
+}

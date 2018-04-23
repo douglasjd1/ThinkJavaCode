f6167111adec2ea86306c211dade7ac229e4bc6d
douglasjd1 <douglasjd09@gmail.com>
2018-04-23 12:53:24 -0500

    Loops7D

---
+++ ch07/Loops7D.java
@@ -0,0 +1,15 @@
+public class Loops7D
+{
+    public static void main(String[] args)
+    {
+        loop(178);
+    }
+
+    private static void loop(int x)
+    {
+        for(int i = 1; i <= x; i++)
+        {
+            System.out.println(i);
+        }
+    }
+}

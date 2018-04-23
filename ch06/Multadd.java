d08ae16d4449146371b386888ad3e40f2f064e8c
douglasjd1 <douglasjd09@gmail.com>
2018-04-23 08:57:13 -0500

    MultAdd

---
+++ ch06/Multadd.java
@@ -0,0 +1,17 @@
+public class Multadd
+{
+    public static void main(String[] args)
+    {
+        double a = 1.0;
+        double b = 2.0;
+        double c = 3.0;
+
+        System.out.println(multadd(a,b,c));
+
+    }
+
+    private static double multadd(double a, double b, double c)
+    {
+        return a * b + c;
+    }
+}

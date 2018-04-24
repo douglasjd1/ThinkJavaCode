aed91ec9927606b566e0d0a883994a99bcc08f62
douglasjd1 <douglasjd09@gmail.com>
2018-04-24 13:16:11 -0500

    Intergalactic Vending Machine Gold version

--- ch08/IntergalacticVendingMachine.java
+++ ch08/IntergalacticVendingMachine.java
@@ -13,16 +13,38 @@ public class IntergalacticVendingMachine
         printMenu(food);
         System.out.println();
 
-        do
+        while(userChoice != 99)
         {
-        System.out.println("Which would you like?");
+            System.out.println("Which would you like?");
 
-        userChoice = in.nextInt();
+            userChoice = in.nextInt();
+            for(int i = 0;i<array(userChoice).length;i++)
+            {
 
-        printSales(sales, food, userChoice);
 
-        }while(userChoice != 99);
+                if(array(userChoice)[i] == 0 || array(userChoice)[i] == 1 ||array(userChoice)[i] == 2)
+                    System.out.println("Thank you for ordering " + food[array(userChoice)[i]]);
 
+                if (array(userChoice)[i] == 0)
+                    sales[0]++;
+                if (array(userChoice)[i] == 1)
+                    sales[1]++;
+                if (array(userChoice)[i] == 2)
+                    sales[2]++;
+                printSales(sales, food);
+                if(i<array(userChoice).length-1)
+                {
+                    if(array(userChoice)[i]==9&&array(userChoice)[i+1]==9)
+                        userChoice = 99;
+                }
+            }
+            System.out.println();
+        }
+        System.out.println("Total sales: ");
+        for(int i = 0;i<sales.length;i++)
+        {
+            System.out.println(sales[i] + " of " + food[i]);
+        }
         System.out.println("Goodbye!");
     }
 
@@ -34,21 +56,38 @@ public class IntergalacticVendingMachine
         }
     }
 
-    private static void printSales(int[] sales, String[] food, int foodBought)
+    private static void printSales(int[] sales, String food[])
     {
-        int totalBought = 0;
+        System.out.println("Sales so far: ");
 
-
-        if (foodBought == 0)
-            sales[0]++;
-        if (foodBought == 1)
-            sales[1]++;
-        if (foodBought == 2)
-            sales[2]++;
-        System.out.println("Sold so far: \n");
         for(int i = 0;i<food.length;i++)
         {
             System.out.println(sales[i] + " of " + food[i]);
         }
+
+    }
+
+    private static int[] array(int input)
+    {
+        int n = 0;
+        int inputb = input;
+
+        while(inputb > 0)
+        {
+            inputb /= 10;
+            n++;
+        }
+        if(input == 0)
+        {
+            n = 1;
+        }
+        int[] selection = new int[n];
+        for(int i = 0;i<selection.length;i++)
+        {
+            selection[i] = input % 10;
+            input/=10;
+        }
+
+        return selection;
     }
 }

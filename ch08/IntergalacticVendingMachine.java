3f2dea4b52e4cb90a7211bee634cabfcac632e15
douglasjd1 <douglasjd09@gmail.com>
2018-04-25 09:01:07 -0500

    Intergalactic Vending Machine Gold version fixed

--- ch08/IntergalacticVendingMachine.java
+++ ch08/IntergalacticVendingMachine.java
@@ -8,37 +8,48 @@ public class IntergalacticVendingMachine
         Scanner in = new Scanner(System.in);
         int[] sales = new int[3];
 
-        int userChoice = -1;
+        String userChoice = "";
         System.out.println("Menu options: ");
         printMenu(food);
         System.out.println();
+        boolean is99 = false;
 
-        while(userChoice != 99)
+        while(!is99)
         {
             System.out.println("Which would you like?");
 
-            userChoice = in.nextInt();
-            for(int i = 0;i<array(userChoice).length;i++)
-            {
+            userChoice = in.nextLine();
 
-
-                if(array(userChoice)[i] == 0 || array(userChoice)[i] == 1 ||array(userChoice)[i] == 2)
-                    System.out.println("Thank you for ordering " + food[array(userChoice)[i]]);
-
-                if (array(userChoice)[i] == 0)
-                    sales[0]++;
-                if (array(userChoice)[i] == 1)
-                    sales[1]++;
-                if (array(userChoice)[i] == 2)
-                    sales[2]++;
-                printSales(sales, food);
-                if(i<array(userChoice).length-1)
+                for (int i = 0; i < array(userChoice).length; i++)
                 {
-                    if(array(userChoice)[i]==9&&array(userChoice)[i+1]==9)
-                        userChoice = 99;
+                    if (array(userChoice)[i] == '0' || array(userChoice)[i] == '1' || array(userChoice)[i] == '2')
+
+                    if (array(userChoice)[i] == '0')
+                    {
+                        sales[0]++;
+                        System.out.println("Thank you for ordering " + food[0]);
+                    }
+                    if (array(userChoice)[i] == '1')
+                    {
+                        sales[1]++;
+                        System.out.println("Thank you for ordering " + food[1]);
+                    }
+                    if (array(userChoice)[i] == '2')
+                    {
+                        System.out.println("Thank you for ordering " + food[2]);
+                        sales[2]++;
+                    }
+                    System.out.println();
+                    printSales(sales, food);
+                    System.out.println();
+                    if (i < array(userChoice).length - 1)
+                    {
+                        if (array(userChoice)[i] == '9' && array(userChoice)[i + 1] == '9')
+                        {
+                            is99=true;
+                        }
+                    }
                 }
-            }
-            System.out.println();
         }
         System.out.println("Total sales: ");
         for(int i = 0;i<sales.length;i++)
@@ -58,36 +69,19 @@ public class IntergalacticVendingMachine
 
     private static void printSales(int[] sales, String food[])
     {
-        System.out.println("Sales so far: ");
+        System.out.println("Sales so far: \n");
 
         for(int i = 0;i<food.length;i++)
         {
-            System.out.println(sales[i] + " of " + food[i]);
+            System.out.println(sales[i] + " " + food[i]);
         }
 
     }
 
-    private static int[] array(int input)
+    private static char[] array(String input)
     {
-        int n = 0;
-        int inputb = input;
+        char[] chars = input.toCharArray();
+            return chars;
 
-        while(inputb > 0)
-        {
-            inputb /= 10;
-            n++;
-        }
-        if(input == 0)
-        {
-            n = 1;
-        }
-        int[] selection = new int[n];
-        for(int i = 0;i<selection.length;i++)
-        {
-            selection[i] = input % 10;
-            input/=10;
-        }
-
-        return selection;
     }
 }

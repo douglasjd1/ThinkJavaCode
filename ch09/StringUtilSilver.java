7ae6c0903952ddc7ab6eb3181536737ca9cbd21f
douglasjd1 <douglasjd09@gmail.com>
2018-04-26 09:30:57 -0500

    StringUtilSilver

--- ch09/StringUtilSilver.java
+++ ch09/StringUtilSilver.java
@@ -1,9 +1,87 @@
+import java.util.Arrays;
+
 public class StringUtilSilver
 {
     public static void main(String[] args)
     {
-        printPhoneNumber("1231231234");
+        printPhoneNumber("565-786-5677");
+        System.out.println("Reverse Hello: " + reverse("Hello"));
+        System.out.println("Is Palindrome: " + isPalindrome(""));
+        System.out.println("Starts with learn: " + allLetters("dog"));
+        String[] names = {"t"};
+        System.out.println("All letters no dupes: " + allLettersNoDupes(names));
+    }
 
+    private static boolean allLettersNoDupes(String[] x)
+    {
+        if(x.length==0)
+            return false;
+        if(x.length==1&&x[0].substring(0, 1).equals("l")||x[0].substring(0, 1).equals("e")||x[0].substring(0, 1).equals("a")||x[0].substring(0, 1).equals("r")||x[0].substring(0, 1).equals("n"))
+            return true;
+
+        int count=0;
+        for(int i=0;i<x.length;i++)
+        {
+            if (x[i].substring(0, 1).equals("l"))
+                count++;
+            if (x[i].substring(0, 1).equals("e"))
+                count++;
+            if (x[i].substring(0, 1).equals("a"))
+                count++;
+            if (x[i].substring(0, 1).equals("r"))
+                count++;
+            if (x[i].substring(0, 1).equals("n"))
+                count++;
+        }
+
+        if(count==1)
+            return true;
+        return false;
+    }
+    private static boolean allLetters(String x)
+    {
+        for(int i=0;i<x.length();i++)
+        {
+            if(x.substring(0,1).equals("l"))
+                return true;
+            if(x.substring(0,1).equals("e"))
+                return true;
+            if(x.substring(0,1).equals("a"))
+                return true;
+            if(x.substring(0,1).equals("r"))
+                return true;
+            if(x.substring(0,1).equals("n"))
+                return true;
+        }
+        return false;
+    }
+    private static boolean isPalindrome(String x)
+    {
+        for(int i=0;i<x.length();i++)
+        {
+            String first = x.substring(i,i+1);
+            String last = x.substring(x.length()-(i+1),x.length()-i);
+            if(!first.equals(last))
+                return false;
+        }
+        return true;
+    }
+
+
+    private static String reverse(String x)
+    {
+        char[] charArray = x.toCharArray();
+        char[] charArrayConverted = new char[x.length()];
+        int stringLength = x.length();
+
+        for(int i=0;i<x.length();i++)
+        {
+            charArrayConverted[i] = charArray[stringLength-1];
+            stringLength--;
+
+        }
+        String result = new String(charArrayConverted);
+        return result;
     }
 
     private static void printPhoneNumber(String x)
@@ -13,49 +91,45 @@ public class StringUtilSilver
         {
             for(int i=0;i<x.length();i++)
             {
-                if(i>=0&&i<=3||i>=4&&i<=7||i>=8&&i<=12)
+                if ((x.substring(i, i + 1)).equals("1"))
                 {
-                    if ((x.substring(i, i + 1)).equals("1"))
-                    {
-                        phoneNumber[i] = 1;
-                    }
-
-                    if ((x.substring(i, i + 1)).equals("a") || (x.substring(i, i + 1)).equals("b") || (x.substring(i, i + 1)).equals("c") || (x.substring(i, i + 1)).equals("2"))
-                    {
-                        phoneNumber[i] = 2;
-                    }
-                    else if ((x.substring(i, i + 1)).equals("d") || (x.substring(i, i + 1)).equals("e") || (x.substring(i, i + 1)).equals("f") || (x.substring(i, i + 1)).equals("3"))
-                    {
-                        phoneNumber[i] = 3;
-                    }
-                    else if ((x.substring(i, i + 1)).equals("h") || (x.substring(i, i + 1)).equals("i") || (x.substring(i, i + 1)).equals("j") || (x.substring(i, i + 1)).equals("4"))
-                    {
-                        phoneNumber[i] = 4;
-                    }
-                    else if ((x.substring(i, i + 1)).equals("k") || (x.substring(i, i + 1)).equals("l") || (x.substring(i, i + 1)).equals("m") || (x.substring(i, i + 1)).equals("5"))
-                    {
-                        phoneNumber[i] = 5;
-                    }
-                    else if ((x.substring(i, i + 1)).equals("n") || (x.substring(i, i + 1)).equals("o") || (x.substring(i, i + 1)).equals("p") || (x.substring(i, i + 1)).equals("6"))
-                    {
-                        phoneNumber[i] = 6;
-                    }
-                    else if ((x.substring(i, i + 1)).equals("q") || (x.substring(i, i + 1)).equals("r") || (x.substring(i, i + 1)).equals("s") || (x.substring(i, i + 1)).equals("7"))
-                    {
-                        phoneNumber[i] = 7;
-                    }
-                    else if ((x.substring(i, i + 1)).equals("t") || (x.substring(i, i + 1)).equals("u") || (x.substring(i, i + 1)).equals("v") || (x.substring(i, i + 1)).equals("8"))
-                    {
-                        phoneNumber[i] = 8;
-                    }
-                    else if ((x.substring(i, i + 1)).equals("x") || (x.substring(i, i + 1)).equals("y") || (x.substring(i, i + 1)).equals("z") || (x.substring(i, i + 1)).equals("9"))
-                    {
-                        phoneNumber[i] = 9;
-                    }
-                    else if ((x.substring(i, i + 1)).equals("+") || (x.substring(i, i + 1)).equals("0"))
-                    {
-                        phoneNumber[i] = 0;
-                    }
+                    phoneNumber[i] = 1;
+                }
+                else if ((x.substring(i, i + 1)).equals("a") || (x.substring(i, i + 1)).equals("b") || (x.substring(i, i + 1)).equals("c") || (x.substring(i, i + 1)).equals("2"))
+                {
+                    phoneNumber[i] = 2;
+                }
+                else if ((x.substring(i, i + 1)).equals("d") || (x.substring(i, i + 1)).equals("e") || (x.substring(i, i + 1)).equals("f") || (x.substring(i, i + 1)).equals("3"))
+                {
+                    phoneNumber[i] = 3;
+                }
+                else if ((x.substring(i, i + 1)).equals("h") || (x.substring(i, i + 1)).equals("i") || (x.substring(i, i + 1)).equals("j") || (x.substring(i, i + 1)).equals("4"))
+                {
+                    phoneNumber[i] = 4;
+                }
+                else if ((x.substring(i, i + 1)).equals("k") || (x.substring(i, i + 1)).equals("l") || (x.substring(i, i + 1)).equals("m") || (x.substring(i, i + 1)).equals("5"))
+                {
+                    phoneNumber[i] = 5;
+                }
+                else if ((x.substring(i, i + 1)).equals("n") || (x.substring(i, i + 1)).equals("o") || (x.substring(i, i + 1)).equals("p") || (x.substring(i, i + 1)).equals("6"))
+                {
+                    phoneNumber[i] = 6;
+                }
+                else if ((x.substring(i, i + 1)).equals("q") || (x.substring(i, i + 1)).equals("r") || (x.substring(i, i + 1)).equals("s") || (x.substring(i, i + 1)).equals("7"))
+                {
+                    phoneNumber[i] = 7;
+                }
+                else if ((x.substring(i, i + 1)).equals("t") || (x.substring(i, i + 1)).equals("u") || (x.substring(i, i + 1)).equals("v") || (x.substring(i, i + 1)).equals("8"))
+                {
+                    phoneNumber[i] = 8;
+                }
+                else if ((x.substring(i, i + 1)).equals("x") || (x.substring(i, i + 1)).equals("y") || (x.substring(i, i + 1)).equals("z") || (x.substring(i, i + 1)).equals("9"))
+                {
+                    phoneNumber[i] = 9;
+                }
+                else if ((x.substring(i, i + 1)).equals("+") || (x.substring(i, i + 1)).equals("0"))
+                {
+                    phoneNumber[i] = 0;
                 }
             }
 
@@ -74,7 +148,7 @@ public class StringUtilSilver
                         phoneNumber[i] = 1;
                     }
 
-                    if ((x.substring(i, i + 1)).equals("a") || (x.substring(i, i + 1)).equals("b") || (x.substring(i, i + 1)).equals("c") || (x.substring(i, i + 1)).equals("2"))
+                    else if ((x.substring(i, i + 1)).equals("a") || (x.substring(i, i + 1)).equals("b") || (x.substring(i, i + 1)).equals("c") || (x.substring(i, i + 1)).equals("2"))
                     {
                         phoneNumber[i] = 2;
                     }
@@ -119,4 +193,6 @@ public class StringUtilSilver
             System.out.println();
         }
     }
+
+
 }

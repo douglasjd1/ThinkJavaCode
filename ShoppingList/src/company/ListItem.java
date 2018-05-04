e35bc7d2df0dc304067371819cbc6158d73df14d
douglasjd1 <douglasjd09@gmail.com>
2018-05-04 10:14:45 -0500

    Shopping list

---
+++ ShoppingList/src/company/ListItem.java
@@ -0,0 +1,33 @@
+package company;
+
+public class ListItem
+{
+    private String name;
+    private int quantity = 1;
+
+    ListItem(String name)
+    {
+        this.name = name;
+        this.quantity = quantity;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public int getQuantity()
+    {
+        return quantity;
+    }
+
+    public void quantityUp()
+    {
+        quantity++;
+    }
+
+    public void quantityDown()
+    {
+        quantity--;
+    }
+}
